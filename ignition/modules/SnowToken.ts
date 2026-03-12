011247d5ab9ea93af5ba0062e122bd7903982bf9
snowflakeworld <snow.miller.frozen@outlook.com>
2026-03-12 12:58:30 -0400

    chore: remove unnecessary

--- ignition/modules/SnowToken.ts
+++ ignition/modules/SnowToken.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const INITIAL_SUPPLY = 10 ** 3;
+// const INITIAL_SUPPLY = 10 ** 3;
 const MAX_SUPPLY = 10 ** 5;
 
 const SnowTokenModule = buildModule("SnowTokenModule", (m) => {
